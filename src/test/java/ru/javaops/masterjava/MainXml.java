ea084de6de36db5fafd459dba5a92db43c139073
dkorolev <ue73062@sbb.ch>
2019-07-11 18:22:50 +0300

    3 2 HW2 JAXB HTML

---
+++ src/test/java/ru/javaops/masterjava/MainXml.java
@@ -0,0 +1,90 @@
+package ru.javaops.masterjava;
+
+import static j2html.TagCreator.body;
+import static j2html.TagCreator.h1;
+import static j2html.TagCreator.head;
+import static j2html.TagCreator.html;
+import static j2html.TagCreator.table;
+import static j2html.TagCreator.td;
+import static j2html.TagCreator.th;
+import static j2html.TagCreator.title;
+import static j2html.TagCreator.tr;
+
+import java.io.InputStream;
+import java.io.Writer;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import com.google.common.io.Resources;
+import j2html.tags.ContainerTag;
+import one.util.streamex.StreamEx;
+import ru.javaops.masterjava.xml.schema.ObjectFactory;
+import ru.javaops.masterjava.xml.schema.Payload;
+import ru.javaops.masterjava.xml.schema.Project;
+import ru.javaops.masterjava.xml.schema.User;
+import ru.javaops.masterjava.xml.util.JaxbParser;
+import ru.javaops.masterjava.xml.util.Schemas;
+
+public class MainXml {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Format: projectName");
+            System.exit(1);
+        }
+        String projectName = args[0];
+        URL payloadUrl = Resources.getResource("payload.xml");
+
+        Set<User> users = parseByJaxb(projectName, payloadUrl);
+        users.forEach(System.out::println);
+
+        String html = toHtml(users, projectName);
+        System.out.println(html);
+        try (Writer writer = Files.newBufferedWriter(Paths.get("out/users.html"))) {
+            writer.write(html);
+        }
+    }
+
+    private static Set<User> parseByJaxb(String projectName, URL payloadUrl) throws Exception {
+        JaxbParser parser = new JaxbParser(ObjectFactory.class);
+        parser.setSchema(Schemas.ofClasspath("payload.xsd"));
+        Payload payload;
+        try (InputStream is = payloadUrl.openStream()) {
+            payload = parser.unmarshal(is);
+        }
+
+        Project project = StreamEx.of(payload.getProjects().getProject())
+                .filter(p -> p.getName().equals(projectName))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Invalid project name '" + projectName + '\''));
+
+        final Set<Project.Group> groups = new HashSet<>(project.getGroup());  // identity compare
+        return StreamEx.of(payload.getUsers().getUser())
+                .filter(u -> !Collections.disjoint(groups, u.getGroupRefs()))
+                .collect(
+                        Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(User::getValue).thenComparing(User::getEmail)))
+                );
+    }
+
+    private static String toHtml(Set<User> users, String projectName) {
+        final ContainerTag table = table().with(
+                tr().with(th("FullName"), th("email")))
+                .attr("border", "1")
+                .attr("cellpadding", "8")
+                .attr("cellspacing", "0");
+
+        users.forEach(u -> table.with(tr().with(td(u.getValue()), td(u.getEmail()))));
+
+        return html().with(
+                head().with(title(projectName + " users")),
+                body().with(h1(projectName + " users"), table)
+        ).render();
+    }
+}
