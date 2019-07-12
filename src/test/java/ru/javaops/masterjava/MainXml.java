74c38b78d2f7413c1472812e2ee8b90962691a52
dkorolev <ue73062@sbb.ch>
2019-07-12 16:02:47 +0300

    3 3 HW2 StAX

--- src/test/java/ru/javaops/masterjava/MainXml.java
+++ src/test/java/ru/javaops/masterjava/MainXml.java
@@ -1,5 +1,6 @@
 package ru.javaops.masterjava;
 
+import static com.google.common.base.Strings.nullToEmpty;
 import static j2html.TagCreator.body;
 import static j2html.TagCreator.h1;
 import static j2html.TagCreator.head;
@@ -10,6 +11,7 @@ import static j2html.TagCreator.th;
 import static j2html.TagCreator.title;
 import static j2html.TagCreator.tr;
 
+import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
 import java.io.Writer;
 import java.net.URL;
@@ -22,6 +24,7 @@ import java.util.Set;
 import java.util.TreeSet;
 import java.util.stream.Collectors;
 
+import com.google.common.base.Splitter;
 import com.google.common.io.Resources;
 import j2html.tags.ContainerTag;
 import one.util.streamex.StreamEx;
@@ -31,12 +34,15 @@ import ru.javaops.masterjava.xml.schema.Project;
 import ru.javaops.masterjava.xml.schema.User;
 import ru.javaops.masterjava.xml.util.JaxbParser;
 import ru.javaops.masterjava.xml.util.Schemas;
+import ru.javaops.masterjava.xml.util.StaxStreamProcessor;
 
 public class MainXml {
 
+    private static final Comparator<User> USER_COMPARATOR = Comparator.comparing(User::getValue).thenComparing(User::getEmail);
+
     public static void main(String[] args) throws Exception {
         if (args.length != 1) {
-            System.out.println("Format: projectName");
+            System.out.println("Required argument: projectName");
             System.exit(1);
         }
         String projectName = args[0];
@@ -45,11 +51,16 @@ public class MainXml {
         Set<User> users = parseByJaxb(projectName, payloadUrl);
         users.forEach(System.out::println);
 
+        System.out.println();
         String html = toHtml(users, projectName);
         System.out.println(html);
         try (Writer writer = Files.newBufferedWriter(Paths.get("out/users.html"))) {
             writer.write(html);
         }
+
+        System.out.println();
+        users = processByStax(projectName, payloadUrl);
+        users.forEach(System.out::println);
     }
 
     private static Set<User> parseByJaxb(String projectName, URL payloadUrl) throws Exception {
@@ -69,8 +80,48 @@ public class MainXml {
         return StreamEx.of(payload.getUsers().getUser())
                 .filter(u -> !Collections.disjoint(groups, u.getGroupRefs()))
                 .collect(
-                        Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(User::getValue).thenComparing(User::getEmail)))
+                        Collectors.toCollection(() -> new TreeSet<>(USER_COMPARATOR))
                 );
+    }
+
+    private static Set<User> processByStax(String projectName, URL payloadUrl) throws Exception {
+
+        try (InputStream is = payloadUrl.openStream()) {
+            StaxStreamProcessor processor = new StaxStreamProcessor(is);
+            final Set<String> groupNames = new HashSet<>();
+
+            // Projects loop
+            projects:
+            while (processor.doUntil(XMLEvent.START_ELEMENT, "Project")) {
+                if (projectName.equals(processor.getAttribute("name"))) {
+                    // Groups loop
+                    String element;
+                    while ((element = processor.doUntilAny(XMLEvent.START_ELEMENT, "Project", "Group", "Users")) != null) {
+                        if (!element.equals("Group")) {
+                            break projects;
+                        }
+                        groupNames.add(processor.getAttribute("name"));
+                    }
+                }
+            }
+            if (groupNames.isEmpty()) {
+                throw new IllegalArgumentException("Invalid " + projectName + " or no groups");
+            }
+
+            // Users loop
+            Set<User> users = new TreeSet<>(USER_COMPARATOR);
+
+            while (processor.doUntil(XMLEvent.START_ELEMENT, "User")) {
+                String groupRefs = processor.getAttribute("groupRefs");
+                if (!Collections.disjoint(groupNames, Splitter.on(' ').splitToList(nullToEmpty(groupRefs)))) {
+                    User user = new User();
+                    user.setEmail(processor.getAttribute("email"));
+                    user.setValue(processor.getText());
+                    users.add(user);
+                }
+            }
+            return users;
+        }
     }
 
     private static String toHtml(Set<User> users, String projectName) {
